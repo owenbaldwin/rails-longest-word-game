32de5649d69f30423a32b0a95b5a3df000e68c3f
Owen Baldwin <owenabaldwin@gmail.com>
2021-11-08 15:15:58 +0000

    done I think

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
 
   def new
@@ -12,7 +14,36 @@ class GamesController < ApplicationController
     @grid
   end
 
+  # def score
+  #   # raise
+  #   url = "https://wagon-dictionary.herokuapp.com/#{params[:answer]}"
+  #   data_fetch = JSON.parse(URI.open(url).read)
+
+  # end
+
+  # def included?(guess, @grid)
+  #   guess.chars.all? { |letter| guess.count(letter) <= @grid.count(letter) }
+  # end
+
+
   def score
+    @answer_array = params[:answer].upcase.chars
+    @grid = params[:grid]
+    if @answer_array.all? { |letter| @answer_array.count(letter) <= @grid.count(letter) }
+      if english_word?(params[:answer])
+        @result = 'well done'
+      else
+        @result = 'not a word'
+      end
+    else
+      @result = 'not in the grid'
+    end
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
   end
 
 end
